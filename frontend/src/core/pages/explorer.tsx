9b4100f08316485da94bce0d0f88ab9c04156583
nemnem202 <naimelhabbas@gmail.com>
2025-10-24 10:26:26 +0200

    implemented tag component

--- frontend/src/core/pages/explorer.tsx
+++ frontend/src/core/pages/explorer.tsx
@@ -2,6 +2,7 @@ import Headline from "../../ui/components/headline";
 import LanguageSelect from "../../ui/components/language_select";
 import Searchbar from "../../ui/components/searchbar";
 import SortingSelect from "../../ui/components/sorting_select";
+import Tag from "../../ui/components/tag";
 
 export default function Explorer() {
   return (
@@ -14,7 +15,14 @@ export default function Explorer() {
           </div>
         }
       />
-      <Searchbar />
+      <div className="flex flex-col items-center gap-2">
+        <Searchbar />
+        <div className="h-5 flex gap-2">
+          {Array.from({ length: 6 }).map((_, id) => (
+            <Tag content={`Tag ${id}`} key={id} />
+          ))}
+        </div>
+      </div>
       <div className="flex gap-5">
         <SortingSelect />
         <LanguageSelect />
