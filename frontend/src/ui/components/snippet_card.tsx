a0157fa3db643d3961c814b78a88171f51e07be1
nemnem202 <naimelhabbas@gmail.com>
2025-10-27 12:36:00 +0100

    added edit and save options in snippet card

--- frontend/src/ui/components/snippet_card.tsx
+++ frontend/src/ui/components/snippet_card.tsx
@@ -2,11 +2,15 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../as
 import SnippetAccordion from "./snippet_accordion";
 import { FaEye } from "react-icons/fa";
 import { FaLink } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
+import { FaSave } from "react-icons/fa";
 
 export default function SnippetCard({
   setClosed,
+  userOrigin = true,
 }: {
   setClosed: React.Dispatch<React.SetStateAction<boolean>>;
+  userOrigin?: boolean;
 }) {
   return (
     <Card className="p-2">
@@ -14,15 +18,19 @@ export default function SnippetCard({
       <CardContent>
         <Card className="min-w-[500px] border-none shadow-none">
           <CardHeader>
-            <div className="flex justify-between items-center w-full">
+            <div className="flex justify-between items-top w-full">
               <div>
                 <CardTitle>Card Title</CardTitle>
                 <CardDescription>Card Description</CardDescription>
               </div>
-              <div className="flex flex-col gap-2">
-                <FaEye className="text-muted-foreground hover:text-secondary transition-colors cursor-pointer" />
-                <FaLink className="text-muted-foreground hover:text-secondary transition-colors cursor-pointer" />
-              </div>
+              {userOrigin && (
+                <div className="flex gap-2">
+                  <FaEye className="text-muted-foreground hover:text-secondary transition-colors cursor-pointer" />
+                  <FaLink className="text-muted-foreground hover:text-secondary transition-colors cursor-pointer" />
+                  <FaEdit className="text-muted-foreground hover:text-secondary transition-colors cursor-pointer" />
+                  <FaSave className="text-muted-foreground hover:text-secondary transition-colors cursor-pointer" />
+                </div>
+              )}
             </div>
           </CardHeader>
           <CardContent>
