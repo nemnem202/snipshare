ed1123a3b39da075169b6c92b5d3a703ca2beb31
nemnem202 <naimelhabbas@gmail.com>
2025-10-28 17:15:04 +0100

    added custom logger

--- backend/src/core/app_bootstrap.ts
+++ backend/src/core/app_bootstrap.ts
@@ -1,11 +1,12 @@
+import { Custom } from "../lib/tools/logger";
 import app from "./app";
 
 const PORT = process.env.PORT;
 
 if (PORT) {
   app.listen(PORT, () => {
-    console.warn("[STARTUP] : app is running on port", PORT, ".");
-    console.warn(`[DOC] : Swagger docs available at http://localhost:${PORT}/docs`);
+    Custom.warn("startup", `app is running on port ${PORT}.`);
+    Custom.warn("doc", `Swagger docs available at http://localhost:${PORT}/docs`);
   });
 } else {
   console.error("[STARTUP] : port is not defined in environnement.");
