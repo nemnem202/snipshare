be2e572c21284fe5ade28845e40b10e4483f133c
nemnem202 <naim.elhabbas@gmail.com>
2025-11-04 15:01:13 +0100

    added middleware for session

--- backend/src/server/lib/middlewares/authMiddleware.ts
+++ backend/src/server/lib/middlewares/authMiddleware.ts
@@ -1,7 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+import CookieParser from "./cookieParser";
+import { ServerResponse } from "../../types/global/response";
 
 export default class AuthMiddleware {
-  static protectUser(req: Request, res: Response, next: NextFunction) {
-    next();
-  }
+  static protectUser = async (req: Request, res: Response<ServerResponse>, next: NextFunction) => {
+    const session = await CookieParser.getSession(req);
+    if (session) return next();
+    return res.json({
+      message: "Cannot access to this feature yet, try to anthenticate !",
+      success: false,
+      redirect: "/login",
+    });
+  };
 }
