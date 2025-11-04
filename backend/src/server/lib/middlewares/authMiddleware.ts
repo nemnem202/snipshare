09f0fd4c46d62b1df5c45ace840c3b2f97ff8f5d
nemnem202 <naim.elhabbas@gmail.com>
2025-11-04 15:06:51 +0100

    upgraded authmiddleware

--- backend/src/server/lib/middlewares/authMiddleware.ts
+++ backend/src/server/lib/middlewares/authMiddleware.ts
@@ -2,10 +2,19 @@ import { NextFunction, Request, Response } from "express";
 import CookieParser from "./cookieParser";
 import { ServerResponse } from "../../types/global/response";
 
+declare module "express" {
+  interface Request {
+    userId?: number;
+  }
+}
+
 export default class AuthMiddleware {
   static protectUser = async (req: Request, res: Response<ServerResponse>, next: NextFunction) => {
     const session = await CookieParser.getSession(req);
-    if (session) return next();
+    if (session) {
+      req.userId = session;
+      return next();
+    }
     return res.json({
       message: "Cannot access to this feature yet, try to anthenticate !",
       success: false,
